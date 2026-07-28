f268c3038c32a5add65725e34f56f7db1fd1e10f
dulip-Jayashanka <dulipj.23@cse.mrt.ac.lk>
2026-07-29 02:55:52 +0530

    2.1.3 done Startupcode
    Startupcode done ...

--- firmware/motor-node/src/main.c
+++ firmware/motor-node/src/main.c
@@ -3,14 +3,16 @@
 int main(void)
 {
     volatile uint32_t counter = 0U;
-    volatile uint32_t hansi = 0U;
+
+    
+
+    static volatile uint32_t dulip = 78;
 
     while (1) {
         counter++;
-        volatile int32_t dulip = 102;
-        dulip ++;
-        hansi = hansi + counter;
-        hansi=hansi +1;
+        dulip++;
+        
+        
     }
 
     return 0;
